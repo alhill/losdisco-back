a55a588fe5fe54627ecdd3a3e66a27320cb7d493
Al Hill <alvarogil91@gmail.com>
2021-04-01 16:38:14 +0200

    Initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '277ce793de98eccbcb8cea08e9376bc0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9b839348-b261-4fbb-9669-1fca9c9e26ac'
+};
